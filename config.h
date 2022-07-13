831bcdb22e693be98b9e7ecda07213df27fde413
mrandr1an <krackedissad@gmail.com>
2022-07-13 21:16:59 +0300

    First commit

--- config.h
+++ config.h
@@ -20,16 +20,22 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = {"1", "2", "3", "4", "煉5",};
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       tags mask     isfloating centerthiswindow  monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,0,           -1 },
+	{ "Firefox",  NULL,       NULL,       0,      0    ,0,           -1 },
+	{ "st",       NULL,       NULL,       5,      0,    1,            -1},
+	{ "alacritty",NULL,       NULL,       0,      0,    1,            -1},
+	{"alacritty -e neofetch", NULL, NULL, 1 << 5,      0,    0,             -1},
+	{"alacritty -e htop", NULL, NULL, 1 << 5,      0,    0,                -1},
+
+
 };
 
 /* layout(s) */
@@ -59,7 +65,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
