6835c40863ab24ee96f27e5e1fc3b91d082429fd
mrandr1an <krackedissad@gmail.com>
2022-07-28 16:50:11 +0300

    Fixed center terminal, changed terminal to kitty and patched with smartborders

--- config.h
+++ config.h
@@ -30,10 +30,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating centerthiswindow  monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,0,           -1 },
 	{ "Firefox",  NULL,       NULL,       0,      0    ,0,           -1 },
-	{ "st",       NULL,       NULL,       5,      0,    1,            -1},
-	{ "alacritty",NULL,       NULL,       0,      0,    1,            -1},
-	{"alacritty -e neofetch", NULL, NULL, 1 << 5,      0,    0,             -1},
-	{"alacritty -e htop", NULL, NULL, 1 << 5,      0,    0,                -1},
+	{ "kitty",       NULL,       NULL,       0,      0,    1,            -1},
 
 
 };
@@ -65,7 +62,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
